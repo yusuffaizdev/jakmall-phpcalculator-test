b6d9c72d11ac976e88135f6e4b289bf41f356588
yusuf faisal <yusuf.faisal@alfabeta.co.id>
2020-07-05 01:33:03 +0700

    v0.3 Make Multiply Command

--- commands.php
+++ commands.php
@@ -3,4 +3,6 @@
 return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
+
 ];
