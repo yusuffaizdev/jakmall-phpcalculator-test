d05bb0c396ae77ef1c37a2ee8e9ad30f4068422f
yusuf faisal <yusuf.faisal@alfabeta.co.id>
2020-07-05 01:35:47 +0700

    v0.4 Make Divide Command

--- commands.php
+++ commands.php
@@ -4,5 +4,5 @@ return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
-
+    \Jakmall\Recruitment\Calculator\Commands\DivideCommand::class,
 ];
